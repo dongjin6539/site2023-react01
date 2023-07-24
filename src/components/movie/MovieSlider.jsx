d7b596718a69e4b69dd04b16e253a87bf91af896
dongjin6539 <dongjin6539@gmail.com>
2023-07-24 13:04:09 +0900

    image

--- src/components/movie/MovieSlider.jsx
+++ src/components/movie/MovieSlider.jsx
@@ -1,72 +1,73 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Autoplay, EffectCoverflow } from 'swiper';
-import 'swiper/scss';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay, EffectCoverflow } from "swiper";
+import "swiper/scss";
 import "swiper/scss/effect-coverflow";
 
 const MovieImg = ({ movie }) => {
-  const renderPoster = () => {
-    if (movie.poster_path) {
-      return (
-        <img
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          alt={movie.title}
-        />
-      );
-    } else {
-      // 대체 이미지 URL 또는 로컬 파일 경로를 지정하세요.
-      return (
-        <img
-          src="../../assets/images/movie/noImage.png"
-          alt={movie.title}
-          style={{ marginLeft: '0' }}
-        />
-      );
-    }
-  };
-  return (
-    <Link to={`https://www.themoviedb.org/movie/${movie.id}`} target='_blank'>
-      {renderPoster()}
-    </Link>
-  )
-}
+    const renderPoster = () => {
+        if (movie.poster_path) {
+            return (
+                <img
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    alt={movie.title}
+                />
+            );
+        } else {
+            // 대체 이미지 URL 또는 로컬 파일 경로를 지정하세요.
+            return (
+                <img
+                    src="../../assets/images/movie/noImage.png"
+                    alt={movie.title}
+                    style={{ marginLeft: "0" }}
+                />
+            );
+        }
+    };
+    return (
+        <Link
+            to={`https://www.themoviedb.org/movie/${movie.id}`}
+            target="_blank"
+        >
+            {renderPoster()}
+        </Link>
+    );
+};
 
 const MovieSlider = ({ movies }) => {
-  return (
-    <div className='movie__slider'>
-      <Swiper
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={5}
-        spaceBetween={30}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        autoplay={{
-          delay: 2000,
-          disableOnInteraction: false,
-        }}
-        pagination={{ clickable: true }}
-        navigation={true}
-        modules={[Autoplay, Navigation, Pagination, EffectCoverflow]}
-      >
-        {movies.map((movie, index) => (
-          <SwiperSlide key={index}>
-            <MovieImg 
-              movie={movie}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
-  )
+    return (
+        <div className="movie__slider">
+            <Swiper
+                effect={"coverflow"}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={5}
+                spaceBetween={30}
+                coverflowEffect={{
+                    rotate: 50,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: true,
+                }}
+                autoplay={{
+                    delay: 2000,
+                    disableOnInteraction: false,
+                }}
+                pagination={{ clickable: true }}
+                navigation={true}
+                modules={[Autoplay, Navigation, Pagination, EffectCoverflow]}
+            >
+                {movies.map((movie, index) => (
+                    <SwiperSlide key={index}>
+                        <MovieImg movie={movie} />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    );
 };
 
 export default MovieSlider;
--- src/components/youtube/YoutubeSlider.jsx
+++ src/components/youtube/YoutubeSlider.jsx
@@ -1,55 +1,60 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Autoplay, EffectCoverflow } from 'swiper';
-import 'swiper/scss';
-import 'swiper/scss/navigation';
-import 'swiper/scss/pagination';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay, EffectCoverflow } from "swiper";
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
 import "swiper/scss/effect-coverflow";
 
 const YoutubeImg = ({ youtube }) => {
-  return (
-    <Link to={`https://www.youtube.com/watch?v=${youtube.id.videoId}`} target='_blank'>
-      <img src={youtube.snippet.thumbnails.medium.url} alt={youtube.snippet.title} style={{ width: '1200px' }}/>
-    </Link>
-  )
-}
+    return (
+        <Link
+            to={`https://www.youtube.com/watch?v=${youtube.id.videoId}`}
+            target="_blank"
+        >
+            <img
+                src={youtube.snippet.thumbnails.high.url}
+                alt={youtube.snippet.title}
+                style={{ width: "1200px" }}
+            />
+        </Link>
+    );
+};
 
 const YoutubeSlider = ({ youtubes }) => {
-  return (
-    <div className='youtube__slider'>
-      <Swiper
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={2}
-        spaceBetween={30}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-        }}
-        pagination={{ clickable: true }}
-        navigation={true}
-        modules={[Autoplay, Navigation, Pagination, EffectCoverflow]}
-      >
-        {youtubes.map((youtube, index) => (
-          <SwiperSlide key={index}>
-            <YoutubeImg 
-              youtube={youtube}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
-  )
+    return (
+        <div className="youtube__slider">
+            <Swiper
+                effect={"coverflow"}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={2}
+                spaceBetween={30}
+                coverflowEffect={{
+                    rotate: 50,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: true,
+                }}
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false,
+                }}
+                pagination={{ clickable: true }}
+                navigation={true}
+                modules={[Autoplay, Navigation, Pagination, EffectCoverflow]}
+            >
+                {youtubes.map((youtube, index) => (
+                    <SwiperSlide key={index}>
+                        <YoutubeImg youtube={youtube} />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    );
 };
 
 export default YoutubeSlider;
